d4cca6b249c8e181e8c3a9beceaddf0bfd864bc1
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2023-07-05 22:32:29 -0700

    working with information card componennt

--- src/pages/Home/InformationCard/InformationCard.tsx
+++ src/pages/Home/InformationCard/InformationCard.tsx
@@ -3,7 +3,16 @@ import "./InformationCard.scss";
 const InformationCard = () => {
   return (
     <>
-      <div className="information_card_container"></div>
+      <div className="information_card">
+        <div className="information_card__container">
+          <div className="information_card__header">
+            <h1>Cruz</h1>
+          </div>
+          <div className="information_card__body">
+            <img src="../../../../public/profile_image.webp" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
