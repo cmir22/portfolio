2ad6fbe993c126ac97d255523d27afeb30d09fba
Cruz Ibarra <jackzelda_vicio@hotmail.com>
2023-07-06 10:28:53 -0700

    Added images in svg

--- src/pages/Home/InformationCard/InformationCard.tsx
+++ src/pages/Home/InformationCard/InformationCard.tsx
@@ -2,6 +2,9 @@ import "./InformationCard.scss";
 import MarkIcon from "../../../assets/Icons/mark.svg";
 import SchoolIcon from "../../../assets/Icons/school.svg";
 import ProfileImage from "../../../../public/profile_image.webp";
+import GihubIcon from "../../../../public/github_icon.svg";
+import LinkedInIcon from "../../../../public/linkedin_icon.svg";
+import GmailIcon from "../../../../public/gmail_icon.svg";
 
 const InformationCard = () => {
   return (
@@ -26,6 +29,13 @@ const InformationCard = () => {
                 <img src={SchoolIcon} />
                 <p>Computer Systems Engineer.</p>
               </div>
+            </div>
+          </div>
+          <div className="information_card__footer_container">
+            <div className="information_card__footer">
+              <img src={GihubIcon} />
+              <img src={LinkedInIcon} />
+              <img src={GmailIcon} />
             </div>
           </div>
         </div>
